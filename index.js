dc5ccc83cfcb7c88b8968173a2c22e29b088f33a
adamrysyk <rysyk.adam@gmail.com>
2020-02-03 21:24:02 -0500

    handle arrow keys

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const canvas = document.getElementById('etch-a-sketch');
 const ctx = canvas.getContext('2d');
 const { width, height } = canvas;
 
+let x = Math.floor(Math.random() * width);
+let y = Math.floor(Math.random() * height);
+
 const shakeButton = document.querySelector('shake');
 
 ctx.lineJoin = 'round';
@@ -9,6 +12,38 @@ ctx.lineCap = 'round';
 ctx.lineWidth = 10;
 
 ctx.beginPath();
-ctx.moveTo(200, 200);
-ctx.lineTo(200, 200);
+ctx.moveTo(x, y);
+ctx.lineTo(x, y);
 ctx.stroke();
+
+const draw = ({ key }) => {
+    console.log(key);
+    ctx.beginPath();
+    ctx.moveTo(x, y)
+    switch(key) {
+        case 'ArrowUp':
+            y -= 10;
+            break;
+        case 'ArrowDown':
+            y += 10;
+            break;
+        case 'ArrowLeft':
+            x -= 10;
+            break;
+        case 'ArrowRight':
+            x += 10;
+            break;
+
+    }
+    ctx.lineTo(x, y);
+    ctx.stroke();
+};
+
+const handleKeyDown = e => {
+    if (e.key.includes('Arrow')) {
+        e.preventDefault();
+        draw({ key: e.key });
+    }
+};
+
+window.addEventListener('keydown', handleKeyDown);
